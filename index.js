7a79cb5cb13632f3d729256142357e7151081586
Jason Hargrove <rijaha@gmail.com>
2021-10-15 08:57:47 -0500

    My first commit.

---
+++ index.js
@@ -0,0 +1,10 @@
+// ===== To Number Sum =====
+array = [3, 5, -4, 8 , 11, 1, -1, 6];
+targetSum = 10;
+// Output: [-1, 11]
+function twoNumberSum(array, targetSum) {
+    console.log(array, targetSum)
+};
+twoNumberSum(1, 2);
+
+  // To log output: cd into the file. In the terminal enter >node index
