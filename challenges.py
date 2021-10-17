90c559d3580639c260d2e1dae1d59a1944ecf9c7
Jason Hargrove <rijaha@gmail.com>
2021-10-17 12:37:26 -0500

    Two number sum using a hash table.

--- challenges.py
+++ challenges.py
@@ -1,12 +1,25 @@
-# O(n^2) time | O(1) space
+
 # ===== To Number Sum =====
 array = [3, 5, -4, 8 , 11, 1, -1, 6];
 targetSum = 10;
 
+# O(n^2) time | O(1) space
+# def twoNumberSum(array, targetSum):
+#     for i in range(len(array) - 1):
+#         firstNum = array[i]
+#         for j in range(i + 1, len(array)):
+#         if firstNmu + secondNum == targetNum:
+#             return [firstNum, secondNum]
+#     return[]
+
+
+# O(n) time | O(n) space
 def twoNumberSum(array, targetSum):
-    for i in range(len(array) - 1):
-        firstNum = array[i]
-        for j in range(i + 1, len(array)):
-        if firstNmu + secondNum == targetNum:
-            return [firstNum, secondNum]
-    return[]
+    nums = {}
+    for num in array:
+        potentialMatch = targetSum = num
+        if targetSum - num in nums:
+            return [potentialMatch, num]
+        else:
+            nums[num] = True
+    retrun []
--- index.js
+++ index.js
@@ -3,18 +3,33 @@
 array = [3, 5, -4, 8 , 11, 1, -1, 6];
 targetSum = 10;
 // Output: [-1, 11]
+// function twoNumberSum(array, targetSum) {
+//   for (let i = 0; i < array.length - 1; i++) {
+//     const firstNum = array[i];
+//     for (let j = i + 1; j < array.length; j++) {
+//       const secondNum = array[j];
+//       if (firstNum + secondNum === targetSum) {
+//         return [firstNum, secondNum];
+//       }
+//     }
+//   }
+//   return [];
+// };
+// console.log(twoNumberSum(array, targetSum));
+
+// O(n) time | O(n) space
 function twoNumberSum(array, targetSum) {
-  for (let i = 0; i < array.length - 1; i++) {
-    const firstNum = array[i];
-    for (let j = i + 1; j < array.length; j++) {
-      const secondNum = array[j];
-      if (firstNum + secondNum === targetSum) {
-        return [firstNum, secondNum];
-      }
+  const nums = {};
+  for (const num of array) {
+    const potentialMatch = targetSum - num;
+    if (potentialMatch in nums) {
+      return [potentialMatch, num];
+    } else {
+      nums[num] = true;
     }
   }
   return [];
-};
+}
 console.log(twoNumberSum(array, targetSum));
 
   // To log output: cd into the file. In the terminal enter >node index
