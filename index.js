c69d2fbfd464a568d864d63a08b8044c60316198
Jason Hargrove <rijaha@gmail.com>
2021-10-18 18:31:10 -0500

    Two number sum using pointers.

--- index.js
+++ index.js
@@ -18,14 +18,33 @@ targetSum = 10;
 // console.log(twoNumberSum(array, targetSum));
 
 // O(n) time | O(n) space
+// function twoNumberSum(array, targetSum) {
+//   const nums = {};
+//   for (const num of array) {
+//     const potentialMatch = targetSum - num;
+//     if (potentialMatch in nums) {
+//       return [potentialMatch, num];
+//     } else {
+//       nums[num] = true;
+//     }
+//   }
+//   return [];
+// }
+// console.log(twoNumberSum(array, targetSum));
+
+// O(nlog(n)) time | O(1) space
 function twoNumberSum(array, targetSum) {
-  const nums = {};
-  for (const num of array) {
-    const potentialMatch = targetSum - num;
-    if (potentialMatch in nums) {
-      return [potentialMatch, num];
-    } else {
-      nums[num] = true;
+  array.sort((a, b) => a - b);
+  let left = 0;
+  let right = array.length - 1;
+  while (left < right) {
+    const currentSum = array[left] + array[right];
+    if (currentSum === targetSum) {
+      return [array[left], array[right]];
+    } else if (currentSum < targetSum) {
+      left++;
+    } else if (currentSum > targetSum) {
+      right--;
     }
   }
   return [];
