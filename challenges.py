102fa082b59a73c6b90d6e56b608f7bb854b7386
Jason Hargrove <rijaha@gmail.com>
2021-10-26 16:04:59 -0500

    Binary Search Tree Construction.

--- challenges.py
+++ challenges.py
@@ -152,17 +152,99 @@
 # ===== Non-Constructible Change =====
 
 # ↓↓↓↓↓ You'll need to uncomment this to run Non-Constructible Change. ↓↓↓↓↓
-coins = [5, 7, 1, 1, 2, 3, 22];
+# coins = [5, 7, 1, 1, 2, 3, 22];
 
 # O(nLogn) time | O(1) space
-def nonConstructibleChange(coins):
-    coins.sort()
+# def nonConstructibleChange(coins):
+#     coins.sort()
+#
+#     currentChangeCreated = 0
+#     for coin in coins:
+#         if coin > currentChangeCreated + 1:
+#             return currentChangeCreated + 1
+#
+#         currentChangeCreated += coin
+#
+#     return currentChangeCreated + 1
 
-    currentChangeCreated = 0
-    for coin in coins:
-        if coin > currentChangeCreated + 1:
-            return currentChangeCreated + 1
 
-        currentChangeCreated += coin
+# ===== BST Construction =====
 
-    return currentChangeCreated + 1
+# ↓↓↓↓↓ You'll need to uncomment this to run Non-Constructible Change. ↓↓↓↓↓
+#
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Average: 0(Log(n)) time | 0(1) space
+    # Worst: 0(n) time | 0(1) space
+    def insert(self, value):
+        currentNode = self
+        while True:
+            if value < currentNode.value:
+                if currentNode.left is None:
+                    currentNode.left = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.left
+            else:
+                if currentNode.right is None:
+                    currentNode.right = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.right
+        return self
+
+    # Average: 0(Log(n)) time | 0(1) space
+    # Worst: 0(n) time | 0(1) space
+    def contains(self, value):
+        currentNode = self
+        while currentNode is not None:
+            if value < currentNode.value:
+                currentNode = currentNode.left
+            elif value > currentNode.value:
+                currentNode = currentNode.right
+            else:
+                return True
+        return False
+
+    # Average: 0(Log(n)) time | 0(1) space
+    # Worst: 0(n) time | 0(1) space
+    def remove(self, value, parentNode = None):
+        currentNode = self
+        while currentNode is not None:
+            if value < currentNode.value:
+                parentNode = currentNode
+                currentNode = currentNode.left
+            elif value > currentNode.value:
+                parentNode = currentNode
+                currentNode = currentNode.right
+            else:
+                if currentNode.left is not None and currentNode.right is not None:
+                    currentNode.value = currentNode.right.getMinValue()
+                    currentNode.right.remove(currentNode.value, currentNode)
+                elif parentNoed is None:
+                    if currentNode.left is not None:
+                        currentNode.value = currentNode.left.value
+                        currentNode.right = currentNode.left.rigth
+                        currentNode.left = currentNode.left.left
+                    elif currentNode.right is not None:
+                        currentNode.value = currentNode.right.value
+                        currentNode.value = currentNode.right.left
+                        currentNode.value = currentNode.right.right
+                    else:
+                        currentNode.value = None
+                elif parentNode.left = currentNode:
+                    parentNode.left = currentNode.left if currentNode.left is not None else currentNode.right
+                elif parentNode.right == currentNode:
+                    parentNode.right = currentNode.left if currentNode.left is not NOne else currentNode.right
+                break
+        return self
+
+def gitMinValue(self):
+    currentNode = self
+    while currentNode.left is not None:
+        currentNode = currentNode.left
+    return currentNode.value
--- index.js
+++ index.js
@@ -237,4 +237,41 @@
 // console.log(+ str);
 
 
+// ===== BST Construction =====
+
+//↓↓↓↓↓ You'll need to uncomment this to run Non-Constructible Change. ↓↓↓↓↓
+
+
+// Do not edit the class below except for
+// the insert, contains, and remove methods.
+// Feel free to add new properties and methods
+// to the class.
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    // Write your code here.
+    // Do not edit the return statement of this method.
+    return this;
+  }
+
+  contains(value) {
+    // Write your code here.
+  }
+
+  remove(value) {
+    // Write your code here.
+    // Do not edit the return statement of this method.
+    return this;
+  }
+}
+
+// Do not edit the line below.
+exports.BST = BST;
+
+
 // To log output: cd into the file. In the terminal enter >node index
