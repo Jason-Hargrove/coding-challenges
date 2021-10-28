30ce05c7421689076181fb740362afbbb741361a
Jason Hargrove <rijaha@gmail.com>
2021-10-28 12:23:26 -0500

    Binary Search Tree Construction. Remove.

--- index.js
+++ index.js
@@ -253,25 +253,91 @@ class BST {
     this.right = null;
   }
 
+  // Average: O(log(n)) time | O(log(n)) space
+  // Worst: O(n) time | O(n) space
   insert(value) {
-    // Write your code here.
+    if (value < this.value) {
+			if (this.left === null) {
+				this.left = new BST(value);
+			} else {
+				this.left.insert(value);
+			}
+		} else {
+			if (this.right === null) {
+				this.right = new BST(value);
+			} else {
+				this.right.insert(value);
+			}
+		}
     // Do not edit the return statement of this method.
     return this;
   }
 
+  // Average: O(log(n)) time | O(log(n)) space
+  // Worst: O(n) time | O(n) space
   contains(value) {
-    // Write your code here.
+    if (value < this.value) {
+			if (this.left === null) {
+				return false;
+			} else {
+				return this.left.contains(value);
+			}
+		} else if (value > this.value) {
+			if (this.right === null) {
+				return false;
+			} else {
+				return this.right.contains(value);
+			}
+		} else {
+			return true;
+		}
   }
 
-  remove(value) {
-    // Write your code here.
+  // Average: O(log(n)) time | O(log(n)) space
+  // Worst: O(n) time | O(n) space
+  remove(value, parent = null) {
+    if (value < this.value) {
+			if (this.left !== null) {
+				this.left.remove(value, this);
+			}
+		} else if (value > this.value) {
+			if (this.right !== null) {
+				this.right.remove(value, this);
+			}
+		} else {
+			if (this.left !== null && this.right !== null) {
+				this.value = this.right.getMinValue();
+				this.right.remove(this.value, this);
+			} else if (parent === null) {
+				if (this.left !== null) {
+					this.value = this.left.value;
+					this.right = this.left.right;
+					this.left = this.left.left;
+				} else if (this.right !== null) {
+					this.value = this.right.value;
+					this.left = this.right.left;
+					this.right = this.right.right;
+				} else {
+					// This is a single-node tree. Do nothing.
+				}
+			} else if (parent.left === this) {
+				parent.left = this.left !== null ? this.left : this.right;
+			} else if (parent.right === this) {
+				parent.right = this.left !== null ? this.left : this.right;
+			}
+		}
     // Do not edit the return statement of this method.
     return this;
+  }
+
+  getMinValue() {
+    if (this.left === null) {
+      return this.value;
+    } else {
+      return this.left.getMinValue();
+    }
   }
 }
-
-// Do not edit the line below.
-exports.BST = BST;
 
 
 // To log output: cd into the file. In the terminal enter >node index
