cbd04c23b74402d15e9ae3553dd2fbabf4c5adf3
Jason Hargrove <rijaha@gmail.com>
2021-10-31 11:40:38 -0500

    MDN A re-introduction to JavaScript. Strings and other types.

--- index.js
+++ index.js
@@ -194,49 +194,6 @@
 // console.log(nonConstructibleChange(coins));
 
 
-// ===== A re-introduction to JavaScript (JS tutorial) =====
-
-// == Numbers ==
-// console.log(3 / 2);
-// console.log(Math.floor(3 / 2));
-// const num = 0.1 + 0.2;
-// console.log(num);
-// console.log(Math.sin(3.5));
-// r = 3.5;
-// var circumference = 2 * Math.PI * r;
-// console.log(circumference);
-// console.log (parseInt('123', 10));
-// const someNum = (parseInt('010', 10));
-// console.log(typeof(someNum));
-// console.log(parseInt('010'));
-// console.log(parseInt('0x10')); // hexadecimal
-// console.log(parseInt('11', 2));
-// console.log(parseFloat('11'));
-// const unary = + '42';
-// console.log(+ '010');
-// console.log(+ '0x10');
-// console.log(unary + " is a " + typeof(unary) + ".");
-// console.log(parseInt('hello', 10));
-// console.log(NaN + 5);
-// console.log(Number.isNaN(NaN));
-// console.log(Number.isNaN('hello'));
-// console.log(Number.isNaN('1'));
-// console.log(Number.isNaN(undefined));
-// console.log(Number.isNaN({}));
-// console.log(Number.isNaN([1]));
-// console.log(Number.isNaN([1, 2]));
-// console.log(isNaN('hello')); // Bad practice to not use Number with isNaN.
-// console.log(1 / 0);
-// console.log(-1 / 0);
-// console.log(isFinite(1 / 0));
-// console.log(isFinite(-Infinity));
-// console.log(isFinite(NaN));
-// let str = "11.2abc";
-// console.log(parseInt(str, 10));
-// console.log(parseFloat(str));
-// console.log(+ str);
-
-
 // ===== BST Construction =====
 
 //↓↓↓↓↓ You'll need to uncomment this to run Non-Constructible Change. ↓↓↓↓↓
---
+++ re-intro-js.js
@@ -0,0 +1,85 @@
+// ===== A re-introduction to JavaScript (JS tutorial) =====
+// Log everyting.
+log = console.log;
+
+// == Numbers ==
+// console.log(3 / 2);
+// console.log(Math.floor(3 / 2));
+// const num = 0.1 + 0.2;
+// console.log(num);
+// console.log(Math.sin(3.5));
+// r = 3.5;
+// var circumference = 2 * Math.PI * r;
+// console.log(circumference);
+// console.log (parseInt('123', 10));
+// const someNum = (parseInt('010', 10));
+// console.log(typeof(someNum));
+// console.log(parseInt('010'));
+// console.log(parseInt('0x10')); // hexadecimal
+// console.log(parseInt('11', 2));
+// console.log(parseFloat('11'));
+// const unary = + '42';
+// console.log(+ '010');
+// console.log(+ '0x10');
+// console.log(unary + " is a " + typeof(unary) + ".");
+// console.log(parseInt('hello', 10));
+// console.log(NaN + 5);
+// console.log(Number.isNaN(NaN));
+// console.log(Number.isNaN('hello'));
+// console.log(Number.isNaN('1'));
+// console.log(Number.isNaN(undefined));
+// console.log(Number.isNaN({}));
+// console.log(Number.isNaN([1]));
+// console.log(Number.isNaN([1, 2]));
+// console.log(isNaN('hello')); // Bad practice to not use Number with isNaN.
+// console.log(1 / 0);
+// console.log(-1 / 0);
+// console.log(isFinite(1 / 0));
+// console.log(isFinite(-Infinity));
+// console.log(isFinite(NaN));
+// let str = "11.2abc";
+// console.log(parseInt(str, 10));
+// console.log(parseFloat(str));
+// console.log(+ str);
+
+// == Strings ==
+// log("hello".length); // 5
+// log("jason".length);
+// const middleName = "Jason";
+// const letterAtPostion0 = middleName.charAt(0);
+// log(letterAtPostion0); // J
+// log(middleName.charAt(4)); // n
+// let name = 'Jason, Hargrove';
+// name = name.replace('Jason', 'Richard');
+// log(name);
+// log('Jason, Hargrove'.replace('Jason', 'Richard')); // Richard Hargrove
+// let daughterName = 'Eliana';
+// daughterName = daughterName.toUpperCase();
+// log(daughterName);
+
+// == Other types ==
+// let str = undefined;
+// log(str); // undefined
+// let str1 = null;
+// log(str1);
+// let emptyStr = "";
+// let zero = 0;
+// let aLie = false;
+// let notANumber = NaN;
+// let aNullValue = null;
+// let notDefined = undefined;
+// let one = 1;
+// log(emptyStr);
+// log(Boolean(emptyStr));
+// log(Boolean(zero));
+// log(Boolean(aLie));
+// log(Boolean(notANumber));
+// log(Boolean(aNullValue));
+// log(Boolean(notDefined));
+// log(Boolean(one));
+
+
+
+
+
+// To log output: cd into the file. In the terminal enter >node re-intro-js
